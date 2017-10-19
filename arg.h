9385c8a4756200d033a100b0ae3dc038bb2d2c66
Mattias Andrée <maandree@kth.se>
2017-10-19 18:16:18 +0200

    Remove unused code from arg.h
    Signed-off-by: Mattias Andrée <maandree@kth.se>

--- arg.h
+++ arg.h
@@ -29,18 +29,6 @@ extern char *argv0;
 						argc_ = argv[0][0];\
 						switch (argc_)
 
-/* Handles obsolete -NUM syntax */
-#define ARGNUM					case '0':\
-						case '1':\
-						case '2':\
-						case '3':\
-						case '4':\
-						case '5':\
-						case '6':\
-						case '7':\
-						case '8':\
-						case '9'
-
 #define ARGALT(SYMBOL)			}\
 				} else if (argv[0][0] == SYMBOL) {\
 					for (brk_ = 0, argv[0]++, argv_ = argv;\
@@ -56,23 +44,5 @@ extern char *argv0;
 					break;\
 				}\
 			}
-
-#define ARGC()		argc_
-
-#define ARGNUMF()	(brk_ = 1, estrtonum(argv[0], 0, INT_MAX))
-
-#define EARGF(x)	((argv[0][1] == '\0' && argv[1] == NULL)?\
-				((x), abort(), (char *)0) :\
-				(brk_ = 1, (argv[0][1] != '\0')?\
-					(&argv[0][1]) :\
-					(argc--, argv++, argv[0])))
-
-#define ARGF()		((argv[0][1] == '\0' && argv[1] == NULL)?\
-				(char *)0 :\
-				(brk_ = 1, (argv[0][1] != '\0')?\
-					(&argv[0][1]) :\
-					(argc--, argv++, argv[0])))
-
-#define LNGARG()	&argv[0][0]
 
 #endif
